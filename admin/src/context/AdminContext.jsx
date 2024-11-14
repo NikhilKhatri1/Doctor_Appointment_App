9564be1391e31cc4e0d5dd09d860ee7e704b4be3
NikhilKhatri1 <nikhilkhattri2@gmail.com>
2024-11-15 01:26:29 +0530

    Updated url in frontend and admin

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -12,7 +12,7 @@ const AdminContextProvider = (props) => {
     const [dashData, setDashData] = useState(false)
 
     // const backendUrl = import.meta.env.VITE_BACKEND_URL
-    const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000"; // Fallback if undefined
+    const backendUrl = import.meta.env.VITE_BACKEND_URL || "https://doctor-appointment-app-grrd.onrender.com"; // Fallback if undefined
     // console.log(backendUrl)
     const getAllDoctors = async () => {
         try {
--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -7,7 +7,7 @@ export const DoctorContext = createContext()
 
 const DoctorContextProvider = (props) => {
     // if backendUrl can;'t fetch file
-    const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000";
+    const backendUrl = import.meta.env.VITE_BACKEND_URL || "https://doctor-appointment-app-grrd.onrender.com";
     const [dToken, setDToken] = useState(localStorage.getItem('dToken') ? localStorage.getItem('dToken') : '')
     const [appointments, setAppointments] = useState([])
     const [dashData, setDashData] = useState(false)
