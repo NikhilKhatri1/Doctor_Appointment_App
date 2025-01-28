7f7c52e902fa5e5cf276de59de4412b352c4b11d
NikhilKhatri1 <nikhilkhattri2@gmail.com>
2025-01-28 23:04:54 +0530

    fixing database url issue

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+    try {
+        const mongoURI = process.env.MONGODB_URI || "mongodb+srv://nikkhatri311:nikkhatri311@cluster0.mq70n.mongodb.net/prescripto";
 
-    mongoose.connection.on('connected', () => console.log("Database Connected"))
-    
-    await mongoose.connect(`${process.env.MONGODB_URI}/prescripto`)
+        mongoose.connection.on('connected', () => console.log("Database Connected"));
+
+        await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log("MongoDB connected successfully");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error.message);
+    }
 }
 
-export default connectDB
+export default connectDB;
