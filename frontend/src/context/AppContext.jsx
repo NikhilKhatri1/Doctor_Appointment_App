d12288df96626ce699b9e4897a4e74a26618ab56
Nikhil <135802677+NikhilKhatri1@users.noreply.github.com>
2025-01-28 21:56:05 +0530

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -8,7 +8,7 @@ const AppContextProvider = (props) => {
 
     const currencySymbol = '$'
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = https://prescripto-backend-2ggt.onrender.com
     // doctor list
     const [doctors, setDoctors] = useState([]);
     // user authentication token
