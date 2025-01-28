2ee85f73d833bf49803ee7e5fd21ef6c40ac448d
Nikhil <135802677+NikhilKhatri1@users.noreply.github.com>
2025-01-28 22:57:36 +0530

    Update mongodb.js

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
 
     mongoose.connection.on('connected', () => console.log("Database Connected"))
     
-    await mongoose.connect(`${process.env.MONGODB_URI}/prescripto`)
+    await mongoose.connect(mongodb+srv://nikkhatri311:nikkhatri311@cluster0.mq70n.mongodb.net)
 }
 
 export default connectDB
