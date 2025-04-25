adb94c83c47f48c225e7910f99c794f344d91c87
NikhilKhatri1 <nikhilkhattri2@gmail.com>
2025-04-25 12:39:35 +0530

    .

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const mongoURI = process.env.MONGODB_URI || "mongodb+srv://nikkhatri311:nikkhatri311@cluster0.mq70n.mongodb.net";
+        const mongoURI = process.env.MONGODB_URI || "mongodb+srv://nikkhatri311:nikkhatri311@cluster0.mq70n.mongodb.net/prescripto";
 
         mongoose.connection.on('connected', () => console.log("Database Connected"));
 
